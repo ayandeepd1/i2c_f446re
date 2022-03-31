a8f63116972ac5e5552f300b199db7d1ae3f1e34
ayandeepd1 <89993737+ayandeepd1@users.noreply.github.com>
2022-03-31 20:26:30 +0530

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -104,44 +104,34 @@ void i2c_read (uint8_t Address, uint8_t *buffer, uint8_t size)
 
 	int remaining = size;
 
-/**** STEP 1 ****/
 	if (size == 1)
 	{
 		I2C1->DR = Address;  			//  send the address
 		while (!(I2C1->SR1 & (1<<1)));  // wait for ADDR bit to set
 
-		/**** STEP 1-b ****/
 		I2C1->CR1 &= ~(1<<10);  // clear the ACK bit
 		uint8_t temp = I2C1->SR1 | I2C1->SR2;  // read SR1 and SR2 to clear the ADDR bit.... EV6 condition
 		I2C1->CR1 |= (1<<9);  // Stop I2C
 
-		/**** STEP 1-c ****/
 		while (!(I2C1->SR1 & (1<<6)));  // wait for RxNE to set
 
-		/**** STEP 1-d ****/
 		buffer[size-remaining] = I2C1->DR;  // Read the data from the DATA REGISTER
 
 	}
 
-/**** STEP 2 ****/
 	else
 	{
-		/**** STEP 2-a ****/
 		I2C1->DR = Address;  //  send the address
 		while (!(I2C1->SR1 & (1<<1)));  // wait for ADDR bit to set
 
-		/**** STEP 2-b ****/
 		uint8_t temp = I2C1->SR1 | I2C1->SR2;  // read SR1 and SR2 to clear the ADDR bit
 
 		while (remaining>2)
 		{
-			/**** STEP 2-c ****/
 			while (!(I2C1->SR1 & (1<<6)));  // wait for RxNE to set
 
-			/**** STEP 2-d ****/
 			buffer[size-remaining] = I2C1->DR;  // copy the data into the buffer
 
-			/**** STEP 2-e ****/
 			I2C1->CR1 |= 1<<10;  // Set the ACK bit to Acknowledge the data received
 
 			remaining--;
@@ -151,10 +141,8 @@ void i2c_read (uint8_t Address, uint8_t *buffer, uint8_t size)
 		while (!(I2C1->SR1 & (1<<6)));  // wait for RxNE to set
 		buffer[size-remaining] = I2C1->DR;
 
-		/**** STEP 2-f ****/
 		I2C1->CR1 &= ~(1<<10);  // clear the ACK bit
 
-		/**** STEP 2-g ****/
 		I2C1->CR1 |= (1<<9);  // Stop I2C
 
 		remaining--;
